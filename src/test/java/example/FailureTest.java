b7e9ec02ec2e072d52f688dfbe843d076e7949e8
Ray Vanderborght <voidstar@gmail.com>
2020-05-09 22:21:23 -0700

    Put failures back

--- src/test/java/example/FailureTest.java
+++ src/test/java/example/FailureTest.java
@@ -1,5 +1,7 @@
 package example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -7,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  */
 public class FailureTest {
 
-//	@Test
+	@Test
 	void success() {
 		assertEquals(50, 40);
 	}
